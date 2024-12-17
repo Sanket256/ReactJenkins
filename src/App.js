daa83cb8a448a54a3c0f2bade42cde59cd62fd48
Sanket256 <devraysanket@gmail.com>
2024-12-17 16:28:40 +0530

    1st Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container" style={"background-color: black"}>
+      <h1>This is Version 1.0</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
